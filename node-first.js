8735effba02cf1107e176df6d1006c8ac69c5c55
Prajgavander09 <pgavander@gammastack.com>
2020-01-27 18:13:06 +0530

    aaded lodash

--- node-first.js
+++ node-first.js
@@ -1,1 +1,8 @@
-console.log("HEY from nodejs");
+var _ = require("lodash");
+
+
+var string = "HEY from nodejs"
+var str1 = _.camelCase(string);
+var str2 = _.lowerCase(string)
+console.log(str1);
+console.log(str2);
